c98bffc383d7f0495d9a123b527bd4974ae45b28
beaglewiggle1004 <beaglewiggle1004@gmail.com>
2019-03-30 20:56:50 +0900

    apply format

--- algorithms/dynamic-programming/20190326_climbing_stairs/climbing_stairs_ans_jtkim.java
+++ algorithms/dynamic-programming/20190326_climbing_stairs/climbing_stairs_ans_jtkim.java
@@ -1,32 +1,32 @@
 class Solution {
-	// Memoization 용 배열
-	int[] nums = null;
+    // Memoization 용 배열
+    int[] nums = null;
 
-	public int climbStairs(int n) {
-		if (nums == null) {
-			nums = new int[n];
-		}
-
-		if (n < 1) {
-			return 0;
-		} else if (n == 1) {
-			return 1;
-		} else if (n == 2) {
-			return 2;
-		} else {
-			if (nums[n - 1] == 0) {
-				// 계단을 1칸 오르는 경우
-				// 경우의 수 중복 계산을 막기 위해 배열에 저장
-				nums[n - 1] = climbStairs(n - 1);
-			}
-			if (nums[n - 2] == 0) {
-				// 계단을 2칸 오르는 경우
-				// 경우의 수 중복 계산을 막기 위해 배열에 저장
-				nums[n - 2] = climbStairs(n - 2);
-			}
-
-			// 두 경우의 합
-			return nums[n - 1] + nums[n - 2];
-		}
+    public int climbStairs(int n) {
+	if (nums == null) {
+	    nums = new int[n];
 	}
+
+	if (n < 1) {
+	    return 0;
+	} else if (n == 1) {
+	    return 1;
+	} else if (n == 2) {
+	    return 2;
+	} else {
+	    if (nums[n - 1] == 0) {
+		// 계단을 1칸 오르는 경우
+		// 경우의 수 중복 계산을 막기 위해 배열에 저장
+		nums[n - 1] = climbStairs(n - 1);
+	    }
+	    if (nums[n - 2] == 0) {
+		// 계단을 2칸 오르는 경우
+		// 경우의 수 중복 계산을 막기 위해 배열에 저장
+		nums[n - 2] = climbStairs(n - 2);
+	    }
+
+	    // 두 경우의 합
+	    return nums[n - 1] + nums[n - 2];
+	}
+    }
 }
